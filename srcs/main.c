aa1b45440159a281d6e789d4798720af9f707cf7
cmace <cmace@student.42.fr>
2022-03-08 13:52:46 +0100

    no free model on wrong vertex and normal type in face

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: cmace <cmace@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 11:09:42 by cmace             #+#    #+#             */
-/*   Updated: 2022/03/07 13:46:18 by cmace            ###   ########.fr       */
+/*   Updated: 2022/03/08 13:37:13 by cmace            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,10 @@ int	main(int argc, char **argv)
 		printf("%s", KEYMESSAGE);
 		loop(env);
 	}
-	if (ret != WRONG_CHAR && ret != RIP_READ && ret != RIP_OPEN && ret != NO_VERTEX)
+	if (ret != WRONG_CHAR && ret != RIP_READ && ret != RIP_OPEN && ret != NO_VERTEX && ret != WRONG_NORMAL_TYPE_IN_FACE && ret != WRONG_VERTEX_TYPE_IN_FACE)
+	{
 		freeAll(&env, ret);
+	}
 	glDeleteTextures(1, &env.texture);
 	glDeleteProgram(env.programId);
 	i = 0;
